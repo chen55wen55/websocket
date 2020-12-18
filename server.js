1b9d852813d047191dc07abc9b076f43075c2a6b
chen55wen55 <chen55wen55@gmail.com>
2020-12-18 22:29:47 +0800

    test socket

--- server.js
+++ server.js
@@ -1,33 +1,24 @@
-//import express 和 ws 套件
-const express = require('express')
-const SocketServer = require('ws').Server
+'use strict';
 
-//指定開啟的 port
-const PORT = 3000
+const express = require('express');
+const { Server } = require('ws');
 
-//創建 express 的物件，並綁定及監聽 3000 port ，且設定開啟後在 console 中提示
+const PORT = process.env.PORT || 3000;
+const INDEX = '/index.html';
+
 const server = express()
-    .listen(PORT, () => console.log(`Listening on ${PORT}`))
+  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-//將 express 交給 SocketServer 開啟 WebSocket 的服務
-const wss = new SocketServer({ server })
+const wss = new Server({ server });
 
-//當 WebSocket 從外部連結時執行
-wss.on('connection', ws => {
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+  ws.on('close', () => console.log('Client disconnected'));
+});
 
-    //連結時執行此 console 提示
-    console.log('Client connected')
-
-	ws.on('message', data => {
-		let clients = wss.clients
-
-        //做迴圈，發送訊息至每個 client
-        clients.forEach(client => {
-            client.send(data)
-        })
-    })
-    //當 WebSocket 的連線關閉時執行
-    ws.on('close', () => {
-        console.log('Close connected')
-    })
-})
+setInterval(() => {
+  wss.clients.forEach((client) => {
+    client.send(new Date().toTimeString());
+  });
+}, 1000);
